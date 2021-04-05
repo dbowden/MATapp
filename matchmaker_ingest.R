ec0fc287d459958a78ec30b5445baf5a627398e0
Dave <dbowden88@gmail.com>
2021-04-05 09:51:39 -0400

    Delete matchmaker_ingest.R

--- matchmaker_ingest.R
+++
@@ -1,8 +0,0 @@
-# merge into a single dataframe with indicators for the original types
-
-# load data
-programs <- read.csv("dbhids_geocoded.csv")
-programs <- subset(programs, select=-c(X))
-
-providers <- read.csv("BupePrescribersData_ToPrint - allnames_June2020.csv")
-
